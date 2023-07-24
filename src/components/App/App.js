a1f2cd62bc15f79d710dfd6c13762ff108a8367d
Kirill <Kirill.pros@gmail.com>
2023-07-24 21:11:16 +0400

    Прижать footer к низу экрана

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="page">
       <div className="page__container">
         <Header />
-          <main>
+          <main className="main">
             <Routes>
               <Route
                 path="/"
