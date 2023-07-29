04fe7dbe63b0dbe420abe2c68b1e09eb53ffa25d
Kirill <Kirill.pros@gmail.com>
2023-07-29 23:26:07 +0400

    Сверстать блок поиска фильмов

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,27 @@
+import "./SearchForm.css";
+
+function SearchForm() {
+  return (
+    <section className="search">
+      <div className="search-form">
+        <input type="text" className="search-form__input" />
+        <button
+          className="search-form__btn"
+          type="button"
+          aria-label="Найти"
+        ></button>
+      </div>
+      <div className="search__container-toggle">
+        <button
+          type="button"
+          aria-label="Переключатель"
+          class="search__toggle search__toggle_inactive"
+        ></button>
+        <p className="search__toggle-name">Короткометражки</p>
+      </div>
+      <div className="search__line"></div>
+    </section>
+  );
+}
+
+export default SearchForm;
