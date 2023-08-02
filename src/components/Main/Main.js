4c57628aed5444a984c27927671ca4e85a85cf16
Kirill <Kirill.pros@gmail.com>
2023-08-03 01:26:21 +0400

    Рефакторинг блока Портфолио

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -14,7 +14,6 @@ function Main() {
       <AboutProject />
       <Techs />
       <AboutMe />
-      <Portfolio />
     </>
   );
 }
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,9 +1,46 @@
+import { Link } from "react-router-dom";
 import "./Portfolio.css";
 
 function Portfolio() {
-  
   return (
-    <></>
+    <div className="portfolio">
+      <h4 className="portfolio__subtitle">Портфолио</h4>
+      <ul className="portfolio__list">
+        <li className="portfolio__list-item">
+          <Link
+            to="#"
+            className="portfolio__link link"
+            target="blank"
+            rel="noreferrer"
+          >
+            Статичный сайт
+            <div className="portfolio__ico"></div>
+          </Link>
+        </li>
+        <li className="portfolio__list-item link">
+          <Link
+            to="#"
+            className="portfolio__link"
+            target="blank"
+            rel="noreferrer"
+          >
+            Адаптивный сайт
+            <div className="portfolio__ico"></div>
+          </Link>
+        </li>
+        <li className="portfolio__list-item">
+          <Link
+            to="#"
+            className="portfolio__link link"
+            target="blank"
+            rel="noreferrer"
+          >
+            Одностраничное приложение
+            <div className="portfolio__ico"></div>
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
