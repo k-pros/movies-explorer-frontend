c5f8b3dadf0f776aedbcfced85c044685de7fb11
Kirill <Kirill.pros@gmail.com>
2023-08-23 18:47:14 +0400

    Добавить навигацию назад на странице 404

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./PageNotFound.css";
 
 function PageNotFound() {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1);
+  }
+
   return (
     <section className="not-found">
       <h2 className="not-found__title">404</h2>
       <p className="not-found__description">Страница не найдена</p>
-      <Link to="/" className="not-found__link link">Назад</Link>
+      <Link onClick={goBack} className="not-found__link link">Назад</Link>
     </section>
   );
 }
